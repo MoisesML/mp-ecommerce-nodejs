61e4f7488447e457a0d3820536ca509e0b90a821
MoisesML <69059086+MoisesML@users.noreply.github.com>
2020-12-19 12:51:45 -0500

    avance

--- app.js
+++ app.js
@@ -1,8 +1,59 @@
 var express = require('express');
 var exphbs  = require('express-handlebars');
 var port = process.env.PORT || 3000
+const mercadopago = require("mercadopago");
 
 var app = express();
+
+mercadopago.configure({
+    access_token : "APP_USR-8208253118659647-112521-dd670f3fd6aa9147df51117701a2082e-677408439",
+    integrator_id : "dev_2e4ad5dd362f11eb809d0242ac130004"
+});
+
+let comprador = {
+    name: "Lalo",
+    surname: "Landa",
+    email: "test_user_46542185@testuser.com",
+    phone: {
+        area_code:"52",
+        number: 5549737300
+    },
+    identification:{
+        type:"DNI",
+        number:"22334445"
+    },
+    address:{
+        zip_code:"03940",
+        street_name:"Insurgentes Sur",
+        street_number:1602
+    }
+};
+
+let metodos_pago = {
+    installments: 6,
+    exclude_payment_methods: [
+        {
+            id: "diner"
+        }
+    ],
+    exclude_payment_types: [
+        {
+            id: "atm"
+        }
+    ]
+};
+
+let back_urls = {};
+
+let preference = {
+    items : [],
+    back_urls : back_urls,
+    payment_methods : metodos_pago,
+    payer : comprador,
+    auto_return : "approved",
+    external_reference : "lazaromoises06@gmail.com",
+    notification_url : ""
+};
  
 app.engine('handlebars', exphbs());
 app.set('view engine', 'handlebars');
@@ -15,8 +66,48 @@ app.get('/', function (req, res) {
     res.render('home');
 });
 
-app.get('/detail', function (req, res) {
+app.get('/detail', async (req, res) => {
+    console.log(req.query)
+    console.log(req.get("host"));
+    let item = {
+        id: "1234",
+        title: req.query.title,
+        description: "Dispositivo móvil de Tienda e-commerce",
+        picture_url: req.get("host")+req.query.img.substring(1),
+        quantity: +req.query.unit,
+        currency_id: "PEN",
+        unit_price: +req.query.price
+    };
+    preference.back_urls = {
+        success: `${req.get("host")}/success`,
+        pending: `${req.get("host")}/pending`,
+        failure: `${req.get("host")}/failure`
+    }
+    preference.items = [];
+    preference.items.push(item);
+    preference.notification_url = `${req.get("host")}/notificaciones`;
+    let respuesta = await mercadopago.preferences.create(preference);
+    console.log(respuesta.body.init_point);
+    req.query.init_point = respuesta.body.init_point;
+    req.query.id = respuesta.body.id;
     res.render('detail', req.query);
+});
+
+app.get("/success", function(req,res){
+    res.render("success", req.query)
+});
+
+app.get("/pending", function(req,res){
+    res.render("pending", req.query)
+});
+
+app.get("/failure", function(req,res){
+    res.render("failure", req.query)
+});
+
+app.post("/notificaciones", function(req, res){
+    console.log(req.query);
+    res.status(200).send("ok")
 });
 
 app.listen(port);
